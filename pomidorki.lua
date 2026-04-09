e081f2918f83f44d74a3648c3d29fed32d991279
pomidorka4896 <dildo148822867@gmail.com>
2026-04-09 23:15:53 +0500

    Create pomidorki.lua

---
+++ pomidorki.lua
@@ -0,0 +1,44 @@
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+
+local Window = Library.CreateLib("Pomidorka Hub", "RJTheme1")
+
+local Tab = Window:NewTab("Player")
+
+local Section = Tab:NewSection("Character")
+
+
+Section:NewSlider("Player Speed", "SliderInfo", 300, 0, function(s)
+   game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+
+Section:NewSlider("Jump Power", "SliderInfo", 300, 0, function(s)
+   game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+end)
+
+Section:NewToggle("Tp tool", "ToggleInfo", function(state)
+    if state then
+mouse = game.Players.LocalPlayer:GetMouse()
+tool = Instance.new("Tool")
+tool.RequiresHandle = false
+tool.Name = "Tp tool"
+tool.Activated:connect(function()
+local pos = mouse.Hit+Vector3.new(0,2.5,0)
+pos = CFrame.new(pos.X,pos.Y,pos.Z)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pos
+end)
+tool.Parent = game.Players.LocalPlayer.Backpack
+    else
+local player = game.Players.LocalPlayer
+local backpack = player.Backpack
+local toolName = "Tp tool"
+
+local tool = backpack:FindFirstChild(toolName)
+if tool then
+    tool:Destroy()
+    print("Инструмент '" .. toolName .. "' удалён из рюкзака.")
+else
+    print("Инструмент '" .. toolName .. "' не найден в рюкзаке.")
+end
+    end
+end)

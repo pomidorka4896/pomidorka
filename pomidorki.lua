33b3339ecc095b9aee64c32f6f889ab32dffb68a
pomidorka4896 <dildo148822867@gmail.com>
2026-04-10 00:36:21 +0500

    pomidorki.lua

--- pomidorki.lua
+++ pomidorki.lua
@@ -42,3 +42,13 @@ else
 end
     end
 end)
+
+Section:NewButton("Inf Jump", "ButtonInfo", function()
+local InfiniteJumpEnabled = true
+
+game:GetService("UserInputService").JumpRequest:Connect(function()
+    if InfiniteJumpEnabled then
+        game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):ChangeState("Jumping")
+    end
+end)
+end)
